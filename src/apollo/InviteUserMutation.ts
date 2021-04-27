527512c3d1703e3c11d31753fc38cee75bee6772
IndyV <28678264+IndyV@users.noreply.github.com>
2021-04-28 00:11:53 +0200

    Add invite user frontend

---
+++ src/apollo/InviteUserMutation.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: InviteUserMutation
+// ====================================================
+
+export interface InviteUserMutation {
+  inviteUser: boolean;
+}
+
+export interface InviteUserMutationVariables {
+  email: string;
+}
--- src/components/dialogs/InviteUser.tsx
+++ src/components/dialogs/InviteUser.tsx
@@ -1,19 +1,36 @@
+import { gql, useMutation } from '@apollo/client';
 import styled from '@emotion/styled';
 import { DialoogProps } from 'dialoog';
 
 import { InviteUser as InviteUserStruct } from '../../../shared/structs/InviteUser';
+import { InviteUserMutation, InviteUserMutationVariables } from '../../apollo/InviteUserMutation';
+import { useNotify } from '../../hooks/useNotify';
 import { Dialog } from '../Dialog';
 import { Button } from '../forms/Button';
 import { Form } from '../forms/Form';
 import { Input } from '../forms/Input';
 
+const mutation = gql`
+  mutation InviteUserMutation($email: String!) {
+    inviteUser(email: $email)
+  }
+`;
+
 export function InviteUser(props: DialoogProps) {
+  const notify = useNotify();
+  const [mutate] = useMutation<InviteUserMutation, InviteUserMutationVariables>(mutation, {
+    onCompleted: () => {
+      notify('Goed gedaan man');
+      setTimeout(props.close);
+    }
+  });
+
   return (
     <Dialog {...props}>
       <StyledTitle>
         Nodig een gebruiker uit
       </StyledTitle>
-      <Form struct={InviteUserStruct} onSubmit={() => {}}>
+      <Form struct={InviteUserStruct} onSubmit={({ email }) => mutate({ variables: { email } })}>
         <StyledContent>
           <Input name="email" label="E-Mail"/>
           <StyledButton text="Nodig uit"/>
