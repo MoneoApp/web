d841ef00a8176cd942a556384fe35b2441caabf1
houfio <26674115+houfio@users.noreply.github.com>
2021-04-28 13:00:05 +0200

    Add device table

--- src/apollo/UserQuery.ts
+++ src/apollo/UserQuery.ts
@@ -7,11 +7,19 @@
 // GraphQL query operation: UserQuery
 // ====================================================
 
+export interface UserQuery_user_devices {
+  __typename: "Device";
+  id: string;
+  model: string;
+  brand: string;
+}
+
 export interface UserQuery_user {
   __typename: "User";
   id: string;
   email: string;
   role: string;
+  devices: UserQuery_user_devices[];
 }
 
 export interface UserQuery {
--- src/pages/admin/users/[id].tsx
+++ src/pages/admin/users/[id].tsx
@@ -9,6 +9,7 @@ import { Row } from '../../../components/layout/Row';
 import { Heading } from '../../../components/navigation/Heading';
 import { roles } from '../../../constants';
 import { useAuthGuard } from '../../../hooks/useAuthGuard';
+import { Table } from '../../../components/users/Table';
 
 const query = gql`
   query UserQuery($id: ID!) {
@@ -16,6 +17,11 @@ const query = gql`
       id
       email
       role
+      devices {
+        id
+        model
+        brand
+      }
     }
   }
 `;
@@ -32,26 +38,40 @@ export default function User() {
     <>
       <Heading text="Gebruiker"/>
       {data?.user && (
-        <Row>
-          <Column sizes={{ phone: 12, laptop: 6 }}>
-            <Form
-              values={{
-                email: data.user.email,
-                role: data.user.role
-              }}
-              onSubmit={console.log}
-            >
-              <Input name="email" label="E-mail"/>
-              <Input as="select" name="role" label="Rol">
-                {Object.entries(roles).map(([key, value]) => (
-                  <option key={key} value={key}>
-                    {value}
-                  </option>
-                ))}
-              </Input>
-            </Form>
-          </Column>
-        </Row>
+        <>
+          <Row>
+            <Column sizes={{ phone: 12, laptop: 6 }}>
+              <Form
+                values={{
+                  email: data.user.email,
+                  role: data.user.role
+                }}
+                onSubmit={console.log}
+              >
+                <Input name="email" label="E-mail"/>
+                <Input as="select" name="role" label="Rol">
+                  {Object.entries(roles).map(([key, value]) => (
+                    <option key={key} value={key}>
+                      {value}
+                    </option>
+                  ))}
+                </Input>
+              </Form>
+            </Column>
+          </Row>
+          <Table
+            data={data.user.devices}
+            keyBy="id"
+            href={(value) => `/admin/devices/${value.id}`}
+            columns={{
+              model: { title: 'Model' },
+              brand: {
+                title: 'Merk',
+                size: '10rem'
+              }
+            }}
+          />
+        </>
       )}
     </>
   );
