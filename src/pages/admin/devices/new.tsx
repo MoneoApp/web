124a939d798625acbcc152758c99a0eaad71ab5c
houfio <26674115+houfio@users.noreply.github.com>
2021-05-09 12:04:20 +0200

    Add big radio component

--- src/pages/admin/devices/new.tsx
+++ src/pages/admin/devices/new.tsx
@@ -1,3 +1,6 @@
+import { faEye, faQrcode } from '@fortawesome/free-solid-svg-icons';
+
+import { BigRadio } from '../../../components/forms/BigRadio';
 import { FileInput } from '../../../components/forms/FileInput';
 import { Form } from '../../../components/forms/Form';
 import { Input } from '../../../components/forms/Input';
@@ -15,6 +18,21 @@ export default function NewDevice() {
             <Input name="model" label="Model"/>
             <Input name="brand" label="Merk"/>
             <FileInput name="image"/>
+            <BigRadio
+              name="type"
+              label="Detectietype"
+              options={[{
+                value: 'STATIC',
+                icon: faQrcode,
+                description: 'Gebruik een QR code als een startpunt voor de overlay. Vaak gebruikt voor stilstaande apparaten.',
+                color: 'red-100'
+              }, {
+                value: 'DYNAMIC',
+                icon: faEye,
+                description: 'Gebruik \'Machine Learning\' om het model en startpunt te herkennen. Vaak gebruikt voor mobiele apparaten.',
+                color: 'green-100'
+              }]}
+            />
           </Form>
         </Column>
       </Row>
