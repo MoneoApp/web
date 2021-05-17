33f65f27f11cc6085161ea0f5ea3b543559fc2da
houfio <26674115+houfio@users.noreply.github.com>
2021-05-17 13:46:24 +0200

    Fix guards

--- server/features/overlays/Query.ts
+++ server/features/overlays/Query.ts
@@ -1,16 +1,10 @@
 import { extendType, list, nullable } from 'nexus';
 
-import { authorized } from '../../guards/authorized';
-import { guard } from '../../utils/guard';
-
 export const OverlayQuery = extendType({
   type: 'Query',
   definition: (t) => {
     t.field('overlays', {
       type: list('Overlay'),
-      authorize: guard(
-        authorized()
-      ),
       resolve: (parent, args, { db }) => db.overlay.findMany()
     });
 
@@ -19,9 +13,6 @@ export const OverlayQuery = extendType({
       args: {
         id: 'ID'
       },
-      authorize: guard(
-        authorized()
-      ),
       resolve: (parent, { id }, { db }) => db.overlay.findUnique({
         where: { id }
       })
