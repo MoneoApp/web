8c97512c97b957b985d727116dc4c9e5d855571c
houfio <26674115+houfio@users.noreply.github.com>
2021-05-23 23:45:29 +0200

    Fix manual step for real this time

--- migrations/20210523212058_order_increment/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE "manualstep_order_seq";
-ALTER TABLE "ManualStep" ALTER COLUMN "order" SET DEFAULT nextval('manualstep_order_seq');
-ALTER SEQUENCE "manualstep_order_seq" OWNED BY "ManualStep"."order";
