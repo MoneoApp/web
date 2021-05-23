d6450cd6e33eb6421aa26fa6b179dd3e053939e8
houfio <26674115+houfio@users.noreply.github.com>
2021-05-23 22:14:38 +0200

    Fix struct

--- shared/structs/UpdateManual.ts
+++ shared/structs/UpdateManual.ts
@@ -5,5 +5,5 @@ import { UpsertManualStep } from './UpsertManualStep';
 export const UpdateManual = object({
   id: string(),
   title: size(string(), 3, 70),
-  steps: array(UpsertManualStep)
+  steps: size(array(UpsertManualStep), 1, Infinity)
 });
