e9885206115f0f3c5d90ade9d00a4b6be5949f37
houfio <26674115+houfio@users.noreply.github.com>
2021-06-20 17:42:04 +0200

    Fix MON-127

---
+++ src/apollo/AdminQuery.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { UserType } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: AdminQuery
+// ====================================================
+
+export interface AdminQuery_me_customer {
+  __typename: "Customer";
+  id: string;
+  name: string;
+}
+
+export interface AdminQuery_me {
+  __typename: "User";
+  id: string;
+  email: string;
+  type: UserType;
+  customer: AdminQuery_me_customer;
+}
+
+export interface AdminQuery {
+  me: AdminQuery_me;
+}
